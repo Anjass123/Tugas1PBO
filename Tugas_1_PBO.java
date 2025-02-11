595529fcaf899815f57d66b724ec950caef0925a
Alvin A.W <anjasnihbosss@gmail.com>
2025-02-11 21:14:03 +0700

    Add files via upload

---
+++ Tugas_1_PBO.java
@@ -0,0 +1,7 @@
+package com.mycompany.tugas_1_pbo;
+
+public class Tugas_1_PBO {
+    public static void main(String[] args) {
+        System.out.println("Hello World, Saya Alvin Al Hafidz Wibowo dengan NIM 2310934, Saya Senang Belajar java");
+    }
+}
